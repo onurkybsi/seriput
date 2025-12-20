09cc28d42df8bfd689ae64655fb15b1e5a3be1d3
Onur Kayabasi <o.kayabasi@outlook.com>
2025-12-20 16:44:18 +0100

    Switch to Java 17 in common module

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -2,9 +2,10 @@ plugins {
     id("seriput-common")
 }
 
+java { toolchain { languageVersion = JavaLanguageVersion.of(17) } }
+
 dependencies {
     api(platform("tools.jackson:jackson-bom:${Versions.JACKSON}"))
-    api("tools.jackson.core:jackson-databind")
     api("tools.jackson.core:jackson-databind")
 
     testImplementation("com.google.guava:guava:${Versions.GUAVA}")
