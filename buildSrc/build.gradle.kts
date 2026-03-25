07131c9eb6f6bc734a8d81aec4dfcf8b78408c92
Onur Kayabasi <o.kayabasi@outlook.com>
2026-03-25 10:12:36 +0100

    Spotless configured

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,5 +7,5 @@ repositories {
 }
 
 dependencies {
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:${Versions.SPOTLESS}")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:7.0.4")
 }
